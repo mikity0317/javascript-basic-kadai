9fa32735eb0537eaa97dd5b3bbf8aafac6799dc1
mikity0317 <142213905+mikity0317@users.noreply.github.com>
2023-10-09 21:32:34 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,20 @@
+const num=300;
+//３と５の倍数の場合
+if(num%3==0&&num%5==0){
+    console.log('3と5の倍数です')
+
+}
+
+//3の倍数の場合
+else if(num%3==0){
+    console.log('3の倍数です')
+}
+
+//5の倍数の場合
+else if(num%5==0){
+    console.log('5の倍数です')
+}
+
+
+//それ以外の場合
+else{}
